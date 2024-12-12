67e8ae63820697c3b81a886a78e289f8d2038998
Gustaxz <guxtavogusteible@gmail.com>
2024-12-12 20:35:26 -0300

    feat: header styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Sidebar } from "@/components/Sidebar"
 import { useScreens } from "@/hooks/screens"
 import { Screens } from "@/state/screens"
+import { UserCircle } from "@phosphor-icons/react"
 import { History } from "./home/History"
 import { Withdraw } from "./home/Withdraw"
 
@@ -10,8 +11,19 @@ export function Home() {
 	return (
 		<div className="flex h-screen w-screen">
 			<Sidebar />
-			<div className="flex justify-center w-full">
-				{screen === Screens.HOME ? <Withdraw /> : <History />}
+			<div className="flex flex-col gap-4 w-full">
+				<div className="flex text-lg justify-between w-full py-8 max-w-5xl mx-auto">
+					<p className="">
+						Seu saldo: <span className="text-green-500">R$ 120,00</span>
+					</p>
+					<div className="flex items-center gap-2">
+						<p>Olá, Gustavo!</p>
+						<UserCircle size={32} weight="fill" className="text-gray-600" />
+					</div>
+				</div>
+				<div className="flex justify-center w-full">
+					{screen === Screens.HOME ? <Withdraw /> : <History />}
+				</div>
 			</div>
 		</div>
 	)
